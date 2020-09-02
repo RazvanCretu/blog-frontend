1c48958acd684301c6d51676cf2d54d773aa3473
Razvy Cretu <razvan.cretu97@gmail.com>
2020-09-03 00:19:59 +0300

    Articles

--- apollo/queries/article/articles.js
+++ apollo/queries/article/articles.js
@@ -1,21 +1,21 @@
 import gql from "graphql-tag";
 
 const ARTICLES_QUERY = gql`
-  query Articles($skip: Int) {
-    articles(sort: "published_at:desc", limit: 5, start: $skip) {
-      title
-      content
-      slug
-      image {
-        url
-      }
-      category {
-        name
-        slug
-      }
-      published_at
+query Posts($skip: Int) {
+  posts(sort: "published_at:desc", limit: 5, start: $skip) {
+    title
+    content
+    slug
+    cover {
+      url
     }
+    category {
+      name
+      slug
+    }
+    published_at
   }
+}
 `;
 
 export default ARTICLES_QUERY;
